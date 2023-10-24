73cf4c1120e446cc7ee7c0a053c8e68e92a2d306
ryan <rsepassi@gmail.com>
2023-10-24 16:45:22 -0700

    Add headers

--- build.zig
+++ build.zig
@@ -13,6 +13,9 @@ pub fn build(b: *std.Build) !void {
     static_lib.addIncludePath(.{.path = "src/libcap/include" });
     static_lib.linkLibC();
     b.installArtifact(static_lib);
+
+    const header = b.addInstallHeaderFile("src/libcap/include/sys/capability.h", "sys/capability.h");
+    b.getInstallStep().dependOn(&header.step);
 }
 
 const c_sources = [_][]const u8 {

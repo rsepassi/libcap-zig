e569837c75a048f2297ce005915b97448d881aa5
ryan <rsepassi@gmail.com>
2023-10-24 15:56:35 -0700

    Build libcap.a

--- build.zig
+++ build.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    _ = b;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const static_lib = b.addStaticLibrary(.{
+        .name = "cap",
+        .target = target,
+        .optimize = optimize,
+    });
+    static_lib.addCSourceFiles(&c_sources, &[_][]const u8{});
+    static_lib.addIncludePath(.{.path = "src/libcap/include" });
+    static_lib.linkLibC();
+    b.installArtifact(static_lib);
 }
+
+const c_sources = [_][]const u8 {
+    "src/libcap/cap_alloc.c",
+    "src/libcap/cap_extint.c",
+    "src/libcap/cap_file.c",
+    "src/libcap/cap_flag.c",
+    "src/libcap/cap_proc.c",
+    "src/libcap/cap_text.c",
+};
